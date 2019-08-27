ed6379d15ecf0122c54a5b01c6b60765bd39c121
@LuisTrabuco <ltrabucco13@live.com>
2019-08-27 15:25:33 -0400

    make greeting

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -4,6 +4,23 @@ const greeting = document.querySelector('.js.greetings');
 const USER_LS = 'currentUser';
 const SHOWING_ON = 'showing'
 
+function saveName(text){
+    localStorage.setItem(USER_LS,text);
+}
+
+
+function handlesSubmit(){
+     event.preventDefault();
+     const currentValue= input.value;
+     paintGreeting(currentValue);
+     saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_ON);
+    form.addEventListener('submit',handlesSubmit)
+}
+
 function paintGreeting(text){
      form.classList.remove(SHOWING_ON);
      greeting.classList.add(SHOWING_ON);
